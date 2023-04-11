a12af3045e0947eaeaf244c016a77b1633061e3e
ataylorEG <ataylor@experientgroup.com>
2023-04-11 17:49:15 -0400

    serve static frontend

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 
 // Create a new token for request body
 morgan.token('body', (req) => {
@@ -130,7 +131,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 // Start the server
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
